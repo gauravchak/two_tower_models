b331c7f35aa79cdb179d5c7b17f5e8930614af4d
Gaurav Chakravorty <gchak207@gmail.com>
2024-01-15 10:42:16 -0800

    moved out some of the complexities into derived classes

--- src/two_tower_base_retrieval.py
+++ src/two_tower_base_retrieval.py
@@ -1,94 +1,22 @@
 """
-This is a specific instance of a two-tower based candidate generator (retrieval)
+This is a baseline implementation of a two-tower based candidate generator (retrieval)
 in a recommender system.
 Ref: https://recsysml.substack.com/p/two-tower-models-for-retrieval-of
+
+In training we create a user embedding from user features. In this example, we are
+ignoring user history features. They will be handled in a follow on derived class.
+We compute item embeddings for the items in the batch and use the user embedding
+and item embeddings to compute a softmax loss. We assume the training data comprises
+of all items impressed by the user. Hence it includes both positive and hard negatives.
+We weight the loss by the net_user_value, which is a linear combination of point-wise
+immediate rewards. Hence the loss is effectively derived only from the "positives", as
+is assumed in two-tower models.
 """
 
-from typing import List
+from typing import List, Tuple
 import torch
-import math
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-class UserHistoryEncoder(nn.Module):
-    """
-    Given user history [B, H, DI], compute a summary using positional embeddings.
-    """
-    def __init__(
-        self,
-        item_id_embedding_dim: int,
-        history_len: int,
-        num_heads: int,
-    ) -> None:
-        super().__init__()
-        self.item_id_embedding_dim = item_id_embedding_dim
-        self.history_len = history_len
-        self.num_heads = num_heads
-
-        # Create positional embeddings of shape [H, DI]
-        self.positional_embeddings = self.positional_encoding(
-            seq_len = history_len,
-            d_model = item_id_embedding_dim
-        )
-        # Reverse the positional encodings since in the user history we 
-        # using this for, the newest item is at the beginning of the sequence.
-        self.positional_embeddings = self.positional_embeddings.flip([0])
-
-        # Create the multi-head attention module
-        # Note: PyTorch's MultiheadAttention expects input shape 
-        # (seq_len=H, batch_size=B, d_model=DI) 
-        # so we have to permute the dimensions when using this.
-        self.multihead_attn = nn.MultiheadAttention(
-            embed_dim=item_id_embedding_dim,
-            num_heads=num_heads
-        )
-
-    def positional_encoding(seq_len, d_model):
-        PE = torch.zeros(seq_len, d_model)
-        for pos in range(seq_len):
-            for i in range(0, d_model, 2):
-                PE[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
-                if i + 1 < d_model:
-                    PE[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
-        return PE
-
-    def forward(
-        self,
-        user_history: torch.Tensor
-    ) -> torch.Tensor:
-        """
-        params:
-            user_history: [B, H, DI] the newest item is assumed to be at
-                the beginning of the sequence.
-
-        returns [B, 2 DI] a summary of the user history
-        """
-        # Add positional encodings to history embeddings
-        # Since positional encodings are [H, DI] and history embeddings are [B, H, DI]
-        # we need to unsqueeze the positional embeddings to [1, H, DI] and add them
-        user_history = user_history + self.positional_embeddings.unsqueeze(0)
-
-        # Compute multi-head attention
-        # Note: PyTorch's MultiheadAttention returns attn_output and 
-        # attn_output_weights, we only keep attn_output.
-        attn_output, _ = self.multihead_attn(
-            query=user_history.permute(1, 0, 2),
-            key=user_history.permute(1, 0, 2),
-            value=user_history.permute(1, 0, 2)
-        )
-
-        # Convert attn_output back to (B, H, DI) format
-        attn_output = attn_output.permute(1, 0, 2)
-
-        # We will only take the first (most recent) item and the mean value
-        first_item = attn_output[:, 0, :].squeeze(1)
-        mean_value = torch.mean(attn_output, dim=1)
-        # Stack the first item and the mean value [B, 2, DI]
-        user_history_summary = torch.stack(
-            [first_item, mean_value], dim=1
-        )
-        return user_history_summary
 
 
 class TwoTowerBaseRetrieval(nn.Module):
@@ -103,7 +31,6 @@ class TwoTowerBaseRetrieval(nn.Module):
         item_features_size: int,
         user_value_weights: List[float],
         knn_module: nn.Module,
-        enable_position_debiasing: bool = False,
     ) -> None:
         """
         params:
@@ -120,38 +47,69 @@ class TwoTowerBaseRetrieval(nn.Module):
                 of long term user satisfaction.
             knn_module: a module that computes the Maximum Inner Product Search (MIPS)
                 over the item embeddings given the user embedding.
-            enable_position_debiasing: when enabled, we will debias the net_user_value
-                by the part explained purely by position.
         """
         super().__init__()
         self.num_items = num_items
         self.user_value_weights = torch.tensor(user_value_weights)  # noqa TODO add device input.
         self.knn_module = knn_module
-        self.enable_position_debiasing = enable_position_debiasing
 
-        # Embedding layers for user and item ids
+        # Create the archs fo user tower
+        # Embedding layers for user id
         self.user_id_embedding_arch = nn.Embedding(
             user_id_hash_size, user_id_embedding_dim)
+        # Create an arch to process the user_features
+        self.user_features_arch = nn.Sequential(
+            nn.Linear(user_features_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, user_id_embedding_dim)
+        )
+        # Create an arch to process the user_tower_input
+        # Input dimension = 
+        #   user_id_embedding_dim from user_id_embedding_arch
+        #   user_id_embedding_dim from user_features_arch
+        self.user_tower_arch = nn.Linear(
+            2 * user_id_embedding_dim + item_id_embedding_dim, 
+            user_id_embedding_dim
+        )
+
+        # Create the archs for item tower
+        # Embedding layers for item id
         self.item_id_embedding_arch = nn.Embedding(
             item_id_hash_size, item_id_embedding_dim)
-        # Create an arch to process the user_features
-        self.user_features_arch = nn.Linear(
-            user_features_size, user_id_embedding_dim)
-        # Create an arch to process the user_tower_input
-        self.user_tower_arch = nn.Linear(
-            2 * user_id_embedding_dim + item_id_embedding_dim, user_id_embedding_dim)
         # Create an arch to process the item_features
-        self.item_features_arch = nn.Linear(
-            item_features_size, item_id_embedding_dim)
+        self.item_features_arch = nn.Sequential(
+            nn.Linear(item_features_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, item_id_embedding_dim)
+        )
         # Create an arch to process the item_tower_input
         self.item_tower_arch = nn.Linear(
             item_id_embedding_dim, item_id_embedding_dim)
-        if self.enable_position_debiasing:
-            # Create an embedding arch to process position
-            self.position_bias_net_user_value = nn.Embedding(
-                num_embeddings=100,
-                embedding_dim=1
-            )
+
+    def process_user_features(
+        self,
+        user_id: torch.Tensor,  # [B]
+        user_features: torch.Tensor,  # [B, IU]
+    ) -> torch.Tensor:
+        """
+        Process the user features to compute the user embedding.
+
+        Args:
+            user_id (torch.Tensor): Tensor containing the user IDs. Shape: [B]
+            user_features (torch.Tensor): Tensor containing the user features. Shape: [B, IU]
+
+        Returns:
+            torch.Tensor: Tensor representing the user embedding. Shape: [B, DU]
+        """
+        # Process user id
+        user_id_embedding = self.user_id_embedding_arch(user_id)  # [B, DU]
+        # Process user features
+        user_features_embedding = self.user_features_arch(user_features)  # [B, DU]
+        # Concatenate the inputs and pass them through a linear layer to compute the user embedding
+        user_tower_input = torch.cat(
+            [user_id_embedding, user_features_embedding], dim=1
+        )
+        return user_tower_input
 
     def compute_user_embedding(
         self,
@@ -160,8 +118,9 @@ class TwoTowerBaseRetrieval(nn.Module):
         user_history: torch.Tensor,  # [B, H]
     ) -> torch.Tensor:
         """
-        Compute the user embedding .
-        params:
+        Compute the user embedding.
+
+        Args:
             user_id: the user id
             user_features: the user features. We are assuming these are all dense features.
                 In practice you will probably want to support sparse embedding features as well.
@@ -169,18 +128,13 @@ class TwoTowerBaseRetrieval(nn.Module):
                 This is a tensor of item ids. Here we are assuming that the history is
                 a fixed length, but in practice you will probably want to support variable
                 length histories. jagged tensors are a good way to do this.
+                This is NOT USED in this implementation. It is handled in a follow on derived class.
+        
+        Returns:
+            torch.Tensor: Tensor containing user embeddings. Shape: [B, DU]
         """
-        # Pass the user history through the item embedding layer
-        user_history_embedding = self.item_id_embedding_arch(user_history)  # [B, H, DI]
-        # Pass the user history through the user history encoder
-        user_history_summary = self.user_history_encoder(user_history_embedding)  # [B, 2 DI]
-        # Process user id
-        user_id_embedding = self.user_id_embedding_arch(user_id)  # [B, DU]
-        # Process user features
-        user_features_embedding = self.user_features_arch(user_features)  # [B, DU]
-        # Concatenate the inputs and pass them through a linear layer to compute the user embedding
-        user_tower_input = torch.cat(
-            [user_id_embedding, user_features_embedding, user_history_summary], dim=1
+        user_tower_input = self.process_user_features(
+            user_id=user_id, user_features=user_features
         )
         # Compute the user embedding
         user_embedding = self.user_tower_arch(user_tower_input)  # [B, DU]
@@ -193,6 +147,13 @@ class TwoTowerBaseRetrieval(nn.Module):
     ) -> torch.Tensor:
         """
         Process item_id and item_features to compute item embeddings.
+
+        Args:
+            item_id (torch.Tensor): Tensor containing item IDs. Shape: [B]
+            item_features (torch.Tensor): Tensor containing item features. Shape: [B, II]
+
+        Returns:
+            torch.Tensor: Tensor containing item embeddings. Shape: [B, DI]
         """
         # Process item id
         item_id_embedding = self.item_id_embedding_arch(item_id)
@@ -203,7 +164,7 @@ class TwoTowerBaseRetrieval(nn.Module):
             [item_id_embedding, item_features_embedding], dim=1
         )
         # Compute the item embedding
-        item_embedding = self.item_tower_arch(item_tower_input)
+        item_embedding = self.item_tower_arch(item_tower_input)  # [B, DI]
         return item_embedding
 
     def forward(
@@ -212,8 +173,17 @@ class TwoTowerBaseRetrieval(nn.Module):
         user_features: torch.Tensor,  # [B, IU]
         user_history: torch.Tensor,  # [B, H]
     ) -> torch.Tensor:
-        """
-        Compute the user embedding and return the top num_items items using the KNN module
+        """This is used for inference.
+
+        Compute the user embedding and return the top num_items items using the KNN module.
+
+        Args:
+            user_id (torch.Tensor): Tensor representing the user ID. Shape: [B]
+            user_features (torch.Tensor): Tensor representing the user features. Shape: [B, IU]
+            user_history (torch.Tensor): Tensor representing the user history. Shape: [B, H]
+
+        Returns:
+            torch.Tensor: Tensor representing the top num_items items. Shape: [B, num_items]
         """
         # Compute the user embedding
         user_embedding = self.compute_user_embedding(
@@ -222,6 +192,25 @@ class TwoTowerBaseRetrieval(nn.Module):
         # Query the knn module to get the top num_items items
         top_items = self.knn_module(user_embedding, self.num_items)  # [B, num_items]
         return top_items
+
+    def debias_net_user_value(
+        self,
+        net_user_value: torch.Tensor,  # [B]
+        position: torch.Tensor,  # [B]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Returns the processed net_user_value and any losses to be added to the loss function.
+
+        Args:
+            net_user_value (torch.Tensor): The net user value tensor of shape [B].
+            position (torch.Tensor): The position tensor of shape [B].
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: A tuple containing the processed net_user_value tensor and any losses to be added to the loss function.
+
+        This is written as a function and not in train_forward to make it easier to
+        implement in a derived class.
+        """
+        return net_user_value, 0
 
     def train_forward(
         self,
@@ -233,11 +222,25 @@ class TwoTowerBaseRetrieval(nn.Module):
         position: torch.Tensor,  # [B]
         labels: torch.Tensor  # [B, T]
     ) -> float:
-        """Compute the loss during training
+        """
+        This function computes the loss during training.
 
-        We are computing a softmax loss and weighting it by the net_user_value.
-        Optionally we are debiasing the net_user_value by the part explained purely by position.
-        To do this we are computing a MSE loss between the net_user_value and position_bias.
+        Args:
+            user_id (torch.Tensor): User IDs. Shape: [B].
+            user_features (torch.Tensor): User features. Shape: [B, IU].
+            user_history (torch.Tensor): User history. Shape: [B, H].
+            item_id (torch.Tensor): Item IDs. Shape: [B].
+            item_features (torch.Tensor): Item features. Shape: [B, II].
+            position (torch.Tensor): Position. Shape: [B].
+            labels (torch.Tensor): Labels. Shape: [B, T].
+
+        Returns:
+            float: The computed loss.
+
+        Notes:
+            - The loss is computed using softmax loss and weighted by the net_user_value.
+            - Optionally, the net_user_value can be debiased by the part explained purely by position.
+            - The loss is clamped to preserve positive net_user_value and normalized between 0 and 1.
         """
         # Compute the user embedding
         user_embedding = self.compute_user_embedding(
@@ -269,32 +272,34 @@ class TwoTowerBaseRetrieval(nn.Module):
         )  # [B]
 
         # Compute the weighted average of the labels using user_value_weights
+        # In the simplest case, assume you have a single label per item.
+        # This label is either 1 or 0 depending on whether the user engaged
+        # with this item when recommended. Then the net_user_value is non zero
+        # actually exactly 1 when the user engaged with the item and 0 otherwise.
         net_user_value = torch.matmul(labels, self.user_value_weights)  # [B]
 
-        # Optionally debias the net_user_value by the part explained purely by position
-        if self.enable_position_debiasing:
-            # Compute the position bias
-            position_bias = self.position_bias_net_user_value(position).squeeze(1)  # [B]
-            # Compute MSE loss between net_user_value and position_bias
-            position_bias_loss = F.mse_loss(
-                input=net_user_value,
-                target=position_bias,
-                reduction="mean"
-            )  # [1]
-            # Compute the net_user_value without position bias
-            net_user_value = net_user_value - position_bias
+        # Optionally debias the net_user_value by the part explained purely 
+        # by position. Not implemented in this version. Hence net_user_value
+        # is unchanged and additional_loss is 0.
+        net_user_value, additional_loss = self.debias_net_user_value(
+            net_user_value, position
+        )  # [B], [1]
 
         # Clamp to only preserve positive net_user_value and
         # normalize net_user_value by the max value of it in batch.
         # This is to ensure that the net_user_value is between 0 and 1.
-        net_user_value = torch.clamp(net_user_value, min=0) / torch.max(net_user_value)  # [B]
+        # This is not strictly necessary, but it makes it easier to interpret 
+        # the loss.
+        net_user_value = torch.clamp(
+            net_user_value, min=0
+        ) / torch.max(net_user_value)  # [B]
 
         # Compute the product of loss and net_user_value
         loss = loss * net_user_value  # [B]
         loss = torch.mean(loss)  # [1]
         # Optionally add the position bias loss to the loss
         if self.enable_position_debiasing:
-            loss = loss + position_bias_loss
+            loss = loss + additional_loss
 
         return loss
 
